7cf8d8adc80b563a24d3d0a68747d3dd8d142b2a
amorimlt <julianeamorim.lt@gmail.com>
2022-03-25 20:15:32 -0300

    finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿int n1, n2;
+
+Console.Write("Digite um numero: ");
+n1 = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Digite mais um numero: ");
+n2 = Convert.ToInt32(Console.ReadLine()!);
+
+Console.WriteLine($"{n1 + n2}");
